02c1749370bf26fe141f2611e3bb4b4d082dde5a
Jesus Gomez Romero <l23640115@piedad.tecnm.mx>
2025-10-30 16:47:52 -0600

    Footer básico

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Home } from './components/home/home';
 import { Header } from './components/header/header';
 import { Login } from './components/login/login';
 import { Vartest } from "./components/vartest/vartest";
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Header, Login, Vartest],
+  imports: [RouterOutlet, Home, Header, Login, Vartest, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
